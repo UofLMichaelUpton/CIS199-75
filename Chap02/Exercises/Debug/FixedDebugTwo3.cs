bea1d7cb3e1fa53d7423bb3b539468268f1328c0
UofLMichaelUpton <59347275+UofLMichaelUpton@users.noreply.github.com>
2020-01-04 12:15:06 -0500

    Add files via upload
    Adding in debug examples from book

---
+++ Chap02/Exercises/Debug/FixedDebugTwo3.cs
@@ -0,0 +1,29 @@
+// This program computes net weekly pay
+// based on hours worked, rate per hour,
+// and 15% withholding
+using System;
+using static System.Console;
+class DebugTwo3
+{
+   static void Main()
+   {
+      const double WITHHOLDING_RATE = .15;
+      string hoursAsString, rateAsString;
+      double hours, rate;
+      double gross, withholding, net;
+      Write("Enter the number of hours you worked this week >> ");
+      hoursAsString = ReadLine();
+      Write("Enter your hourly rate >> ");
+      rateAsString = ReadLine();
+      hours = Convert.ToDouble(hoursAsString);
+      rate = Convert.ToDouble(rateAsString);
+      gross = hours * rate;
+      withholding = gross * WITHHOLDING_RATE;
+      net = gross - withholding;
+      WriteLine("You worked {0} hours at {1} per hour",
+	 hours, rate.ToString("C2"));
+      WriteLine("Gross pay is {0}", gross.ToString("C2"));
+      WriteLine("Withholding is {0}", withholding.ToString("C2"));
+      WriteLine("Net pay is {0}", net.ToString("C2")); 
+   }
+}
