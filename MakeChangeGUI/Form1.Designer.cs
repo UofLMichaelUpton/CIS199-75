aade7a7504524f6bc1cd4b193c664bf7dcbe2a00
UofLMichaelUpton <59347275+UofLMichaelUpton@users.noreply.github.com>
2020-01-05 17:59:10 -0500

    Add files via upload
    Add MakeChangeGUI to master branch

---
+++ MakeChangeGUI/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MakeChangeGUI
+{
+    partial class makeChangeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterLabel = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.calculateChangeButton = new System.Windows.Forms.Button();
+            this.displayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterLabel
+            // 
+            this.enterLabel.AutoSize = true;
+            this.enterLabel.Enabled = false;
+            this.enterLabel.Location = new System.Drawing.Point(43, 76);
+            this.enterLabel.Name = "enterLabel";
+            this.enterLabel.Size = new System.Drawing.Size(218, 13);
+            this.enterLabel.TabIndex = 0;
+            this.enterLabel.Text = "Enter a whole dollar amount to make change";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(268, 68);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(67, 20);
+            this.amountTextBox.TabIndex = 0;
+            // 
+            // calculateChangeButton
+            // 
+            this.calculateChangeButton.Location = new System.Drawing.Point(252, 94);
+            this.calculateChangeButton.Name = "calculateChangeButton";
+            this.calculateChangeButton.Size = new System.Drawing.Size(101, 23);
+            this.calculateChangeButton.TabIndex = 1;
+            this.calculateChangeButton.Text = "Calculate Change";
+            this.calculateChangeButton.UseVisualStyleBackColor = true;
+            this.calculateChangeButton.Click += new System.EventHandler(this.calculateChangeButton_Click);
+            // 
+            // displayLabel
+            // 
+            this.displayLabel.AutoSize = true;
+            this.displayLabel.Enabled = false;
+            this.displayLabel.Location = new System.Drawing.Point(252, 124);
+            this.displayLabel.Name = "displayLabel";
+            this.displayLabel.Size = new System.Drawing.Size(0, 13);
+            this.displayLabel.TabIndex = 2;
+            // 
+            // makeChangeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 287);
+            this.Controls.Add(this.displayLabel);
+            this.Controls.Add(this.calculateChangeButton);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.enterLabel);
+            this.Name = "makeChangeForm";
+            this.Text = "Make Change";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterLabel;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Button calculateChangeButton;
+        private System.Windows.Forms.Label displayLabel;
+    }
+}
+
---
+++ MakeChangeGUI/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MakeChangeGUI
+{
+    public partial class makeChangeForm : Form
+    {
+        public makeChangeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateChangeButton_Click(object sender, EventArgs e)
+        {
+            const byte TWENTY = 20,  //constant for twenty dollar bill
+                       TEN = 10,  //constant for ten dollar bill
+                       FIVE = 5,  //constant for five dollar bill
+                       ONE = 1;  //constant for one dollar bill
+            int twenties,  //variable for how many twenties there are
+                tens,  //variable for how many tens there are
+                fives,  //variable for how many fives there are
+                ones,  //variable for how many ones there are
+                total,  //variable for user-entered total to make change for
+                newTotal;  //variable for the new total after each bill type is totalled and taken out
+
+            //convert the amount entered by the user to an integer
+            total = Convert.ToInt32(amountTextBox.Text);
+
+            //determine the amount of twenty dollar bills and create a new total
+            twenties = total / TWENTY;
+            newTotal = total % TWENTY;
+
+            //determine the amount of ten dollar bills and create a new total
+            tens = newTotal / TEN;
+            newTotal %= TEN;
+
+            //determine the amount of five dollar bills and create a new total
+            fives = newTotal / FIVE;
+            newTotal %= FIVE;
+
+            //determine the amount of one dollar bills
+            ones = newTotal / ONE;
+
+            //display the results to the user in the displayLabel
+            displayLabel.Text = twenties + " $20 bills\n" +
+                                tens + " $10 bills\n" +
+                                fives + " $5 bills\n" +
+                                ones + " $1 bills";
+        }
+    }
+}
